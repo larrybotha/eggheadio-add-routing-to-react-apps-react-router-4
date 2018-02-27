3fc88aab1136a8209a05f006c857185f7fa2198d
Larry Botha <larry@fixate.it>
2018-02-27 09:10:23 +0100

    whitespace

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ const App = () => (
   <Router>
     <div>
       {/*
-	We need this home path to be exact, because React Router's regex engine
-	evaluates / and /about to be the same
-	The result is that both routes are rendered.
-	'exact' is known as a qualifier. 'exact' doesn't care about trailing
-	forward slashes, but there is another qualifier that does...
+        We need this home path to be exact, because React Router's regex engine
+        evaluates / and /about to be the same
+        The result is that both routes are rendered.
+        'exact' is known as a qualifier. 'exact' doesn't care about trailing
+        forward slashes, but there is another qualifier that does...
       */}
       <Route exact path="/" component={Home} />
 
       {/*
-	The qualifier 'strict' requires that if the path has a trailing slash or
-	not, that accessing the route via the client must be done in the same way
+        The qualifier 'strict' requires that if the path has a trailing slash or
+        not, that accessing the route via the client must be done in the same way
       */}
       {/* <Route strict path="/about/" component={Home} /> */}
 
@@ -25,11 +25,11 @@ const App = () => (
       {/* <Route path="/about" render={() => <h1>About</h1> } /> */}
 
       {/*
-	children works similarly to render, except that it will render
-	regardless of the route.
-	We can use the match prop passed through to the child component
-	to render the route only when the route matches.
-	The 'match' prop is only populated when a route matches the path.
+        children works similarly to render, except that it will render
+        regardless of the route.
+        We can use the match prop passed through to the child component
+        to render the route only when the route matches.
+        The 'match' prop is only populated when a route matches the path.
       */}
       <Route path="/about" children={({match}) => match && <h1>About</h1>} />
     </div>
