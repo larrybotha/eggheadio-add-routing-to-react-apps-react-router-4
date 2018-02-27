8f8845e3707d15b29b44e4fc9d6a38de1b4095db
Larry Botha <larry@fixate.it>
2018-02-27 09:09:38 +0100

    explain the use of the 'exact' qualifier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import {BrowserHistory} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-const App = () => <h1>Hello World</h1>;
+const Home = () => <h1>Home</h1>;
+
+const App = () => (
+  <Router>
+    <div>
+      {/*
+	We need this home path to be exact, because React Router's regex engine
+	evaluates / and /about to be the same
+	The result is that both routes are rendered.
+	'exact' is known as a qualifier. 'exact' doesn't care about trailing
+	forward slashes, but there is another qualifier that does...
+      */}
+      <Route exact path="/" component={Home} />
+
 
 export default App;
