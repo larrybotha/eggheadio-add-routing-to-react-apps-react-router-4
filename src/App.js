fee14a395345a5e0801e3c71c2a24fefd07ff475
Larry Botha <larry@fixate.it>
2018-02-28 09:20:04 +0100

    add annotations for params, optional params, and non-slash delimited params

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 const App = () => (
   <Router>
     <div>
-      <Route path="/page" render={() => <h1>Page</h1>} />
+      {/*
+        ? makes params optional
+
+        We aren't only restricted to subpaths - we could delimit the params
+        using some other delimeter, such as a hyphen:
+        /:page?-:subpage?
+      */}
+      <Route
+        path="/:page?/:subpage?"
+        render={({match}) => (
+          <h1>
+            Page: {match.params.page || 'Home'}
+            <br />
+            SubPage: {match.params.subpage}
+          </h1>
+        )}
+      />
     </div>
   </Router>
 );
