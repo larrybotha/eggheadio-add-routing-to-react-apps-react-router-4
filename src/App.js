ba45fef7f668caf86ed47698aefbbd6f81788556
Larry Botha <larry@fixate.it>
2018-03-01 08:40:04 +0100

    prepare app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 const App = () => (
   <Router>
     <div>
-      {/*
-        ? makes params optional
-
-        We aren't only restricted to subpaths - we could delimit the params
-        using some other delimeter, such as a hyphen:
-        /:page?-:subpage?
-      */}
       <Route
-        path="/:page?/:subpage?"
+        path="/:a/:b"
         render={({match}) => (
           <h1>
-            Page: {match.params.page || 'Home'}
+            paramA: {match.params.a}
             <br />
-            SubPage: {match.params.subpage}
+            paramB: {match.params.b}
           </h1>
         )}
       />
