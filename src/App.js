392741999b65e61647e0c8e8671b38cd1fb4149a
Larry Botha <larry@fixate.it>
2018-03-01 08:58:26 +0100

    show output of query strings on links

--- src/App.js
+++ src/App.js
@@ -23,19 +23,18 @@ const Links = () => (
 const App = () => (
   <Router>
     <div>
-      {/*
-        by usin parens we can add regular expression to matching our route
-        params.
-        If the regex doesn't match the param, the route won't be shown
-      */}
+      <Links />
       <Route
-        path="/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)"
-        render={({match}) => (
-          <h1>
-            paramA: {match.params.a}
-            <br />
-            paramB: {match.params.b}
-          </h1>
+        path="/"
+        render={({match, location}) => (
+          <div>
+            <p>root</p>
+            {/*
+              React Router doesn't provide a mechanism for parsing query strings
+            */}
+            <p>{JSON.stringify(match)}</p>
+            <p>{JSON.stringify(location)}</p>
+          </div>
         )}
       />
     </div>
