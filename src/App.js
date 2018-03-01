76eee62e4f0994079b9e2beca105d8456eb2d533
Larry Botha <larry@fixate.it>
2018-03-01 08:44:23 +0100

    add regex example

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 const App = () => (
   <Router>
     <div>
+      {/*
+        by usin parens we can add regular expression to matching our route
+        params.
+        If the regex doesn't match the param, the route won't be shown
+      */}
       <Route
-        path="/:a/:b"
+        path="/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)"
         render={({match}) => (
           <h1>
             paramA: {match.params.a}
