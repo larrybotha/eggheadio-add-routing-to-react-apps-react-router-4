a6f52d00812c6e5d5394d37bba55f89921fccae7
Larry Botha <larry@fixate.it>
2018-03-04 14:28:13 +0100

    if not inside a Switch, a route without a path will always render

--- src/App.js
+++ src/App.js
@@ -14,17 +14,21 @@ const App = () => (
     <div>
       <Links />
       {/*
-	Switch will only render the first route that matches
+        Switch will only render the first route that matches
       */}
       <Switch>
-	<Route exact path="/" render={() => <h1>Home</h1>} />
-	<Route path="/about" render={() => <h1>About</h1>} />
-	<Route
-	  render={() => (
-	    <h1>Will only render inside Switch if no other routes match</h1>
-	  )}
-	/>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route path="/about" render={() => <h1>About</h1>} />
+        <Route
+          render={() => (
+            <h1>Will only render inside Switch if no other routes match</h1>
+          )}
+        />
       </Switch>
+
+      <Route
+        render={() => <h1>Route without a 'path' prop always renders</h1>}
+      />
     </div>
   </Router>
 );
