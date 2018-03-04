e351504efcecf1f689cf2e305dfd3d27533706e7
Larry Botha <larry@fixate.it>
2018-03-04 14:29:49 +0100

    prepare app

--- src/App.js
+++ src/App.js
@@ -13,22 +13,11 @@ const App = () => (
   <Router>
     <div>
       <Links />
-      {/*
-        Switch will only render the first route that matches
-      */}
       <Switch>
         <Route exact path="/" render={() => <h1>Home</h1>} />
         <Route path="/about" render={() => <h1>About</h1>} />
-        <Route
-          render={() => (
-            <h1>Will only render inside Switch if no other routes match</h1>
-          )}
-        />
+        <Route path="/contact" render={() => <h1>Contact</h1>} />
       </Switch>
-
-      <Route
-        render={() => <h1>Route without a 'path' prop always renders</h1>}
-      />
     </div>
   </Router>
 );
