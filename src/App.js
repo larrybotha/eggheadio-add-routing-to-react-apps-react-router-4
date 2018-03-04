7dda11dd0bbdc587b45f4bdeea7cc0bd6e8d7b21
Larry Botha <larry@fixate.it>
2018-03-04 14:27:51 +0100

    switch will only ever render the first route that matches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 const Links = () => (
   <nav>
@@ -13,8 +13,18 @@ const App = () => (
   <Router>
     <div>
       <Links />
-      <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route path="/about" render={() => <h1>About</h1>} />
+      {/*
+	Switch will only render the first route that matches
+      */}
+      <Switch>
+	<Route exact path="/" render={() => <h1>Home</h1>} />
+	<Route path="/about" render={() => <h1>About</h1>} />
+	<Route
+	  render={() => (
+	    <h1>Will only render inside Switch if no other routes match</h1>
+	  )}
+	/>
+      </Switch>
     </div>
   </Router>
 );
