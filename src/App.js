54a310a955a87746c5465ff99984409924eceff6
Larry Botha <larry@fixate.it>
2018-03-05 08:41:30 +0100

    prepare app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-const Links = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Header />
-    <Content />
-  </nav>
-);
-
-// Route is just a component, and so can be used anywhere as a descendant of
-// Router
-const Header = () => (
-  <div className="header">
-    <Route
-      exact
-      path="/:page"
-      render={({match}) => <h1>{match.params.page} header</h1>}
-    />
-  </div>
-);
-
-// We can render another Route component for the same path again
-const Content = () => (
-  <div className="content">
-    <Route
-      path="/:page"
-      render={({match}) => <p>{match.params.page} content</p>}
-    />
-  </div>
-);
+const Home = () => <h1>Home</h1>;
+const Menu = () => <h1>Menu</h1>;
 
 const App = () => (
   <Router>
     <div>
-      <Links />
+      <Route exact path="/" component={Home} />
+      <Route path="/menu" component={Menu} />
     </div>
   </Router>
 );
