9452bb1dfa098afda98cd6356e3334e7572a2852
Larry Botha <larry@fixate.it>
2018-03-05 08:46:17 +0100

    add notes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const Home = () => <h1>Home</h1>;
-const Menu = () => <h1>Menu</h1>;
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
+
+    <Link to="/menu/food">Food</Link>
+    <Link to="/menu/drinks">Drinks</Link>
+    <Link to="/menu/sides">Sides</Link>
+
+    {/*
+      Nest a route inside this component that will match the above
+    */}
+    <Route
+      path="/menu/:section"
+      render={({match}) => <h2>{match.params.section}</h2>}
+    />
+  </div>
+);
 
 const App = () => (
   <Router>
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Menu</Link>
       <Route exact path="/" component={Home} />
       <Route path="/menu" component={Menu} />
     </div>
