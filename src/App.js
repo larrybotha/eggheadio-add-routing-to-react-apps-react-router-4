2fcdf07bfb48b1ac14a0efa6dcc43c2c6773464a
Larry Botha <larry@fixate.it>
2018-03-06 08:57:14 +0100

    prepare app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Redirect, Router, Route, Link} from 'react-router-dom';
 
-const Home = () => <h1>Home</h1>;
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
-
-    <Link to="/menu/food">Food</Link>
-    <Link to="/menu/drinks">Drinks</Link>
-    <Link to="/menu/sides">Sides</Link>
-
-    {/*
-      Nest a route inside this component that will match the above
-    */}
-    <Route
-      path="/menu/:section"
-      render={({match}) => <h2>{match.params.section}</h2>}
-    />
-  </div>
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/old">Old</Link>
+    <Link to="/new">New</Link>
+  </nav>
 );
 
 const App = () => (
   <Router>
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/menu">Menu</Link>
-      <Route exact path="/" component={Home} />
-      <Route path="/menu" component={Menu} />
+      <Links />
+      <Route path="/" render={() => <h1>Home</h1>} />
+      <Route path="/new" render={() => <h1>New</h1>} />
     </div>
   </Router>
 );
