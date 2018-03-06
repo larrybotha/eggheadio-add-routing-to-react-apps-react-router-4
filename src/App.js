a299cb42a082bc21c689f7d00e1dd8ecf8842790
Larry Botha <larry@fixate.it>
2018-03-06 09:12:51 +0100

    demonstrate redirect as render prop with query params

--- src/App.js
+++ src/App.js
@@ -15,6 +15,28 @@ const App = () => (
       <Links />
       <Route path="/" render={() => <h1>Home</h1>} />
       <Route path="/new" render={() => <h1>New</h1>} />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route
+	path="/new/:str"
+	render={({match}) => <h1>New: {match.params.str}</h1>}
+      />
+
+      {/*
+	  This needs to be inside a Switch when it is being rendered alongside
+	  other Route components so that only 1 route is rendered.
+	  It also rewrites history so that going back will not attempt to
+	  access this route again.
+	  <Redirect from="/old" to="new" />
+
+	  Alternative to using the Switch statement, we can render the Redirect
+	  inside a Route. We can remove the Switch statement in this case.
+
+	  We can also pass query params through to th redirect.
+	*/}
+      <Route
+	path="/old/:str"
+	render={({match}) => <Redirect to={`/new/${match.params.str}`} />}
+      />
     </div>
   </Router>
 );
