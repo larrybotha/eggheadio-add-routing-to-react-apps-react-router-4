f91d5a194e0383c44a9e02e5e1a19b249c66595c
Larry Botha <larry@fixate.it>
2018-03-07 08:40:46 +0100

    prepare app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Prompt} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const Links = () => (
   <nav>
@@ -8,38 +8,12 @@ const Links = () => (
   </nav>
 );
 
-const Home = () => <h1>Home</h1>;
-class Form extends React.Component {
-  state = {dirty: false};
-
-  handleInput = () => {
-    this.setState({dirty: true});
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Form</h1>
-
-        <form>
-          {/*
-            Will show prompt if route changes and 'when' is true
-          */}
-          <Prompt when={this.state.dirty} message="Lose unsaved changes?" />
-
-          <input onInput={this.handleInput} />
-        </form>
-      </div>
-    );
-  }
-}
-
 const App = () => (
   <Router>
     <div>
       <Links />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/form" component={Form} />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route exact path="/about" render={() => <h1>About</h1>} />
     </div>
   </Router>
 );
